73a89de37e5d6459a856c2c2d12d7603077aa748
loki <gunlinux@ya.ru>
2024-05-22 02:48:32 +1000

    passing mysql

--- freepbx/prelaunch.sh
+++ freepbx/prelaunch.sh
@@ -12,5 +12,30 @@ echo 'Database started'
 
 echo  ${MARIADB_USER}
 
+
+database_exists() {
+    local database_name="$1"
+    local output=$(mysql -u "$DB_USER" -h db -p"${DB_PASSWORD}" -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$database_name'")
+    if [ -z "$output" ]; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
+# Check if the 'asterisk' database exists
+if [ "$(database_exists 'asterisk')" == "false" ]; then
+    echo "Database 'asterisk' does not exist. Loading dump..."
+
+    # Load the database dump
+    mysql -h db -u "$DB_USER" -p"${DB_PASSWORD}" < /opt/dump.sql
+
+    echo "Database dump loaded successfully."
+else
+    echo "Database 'asterisk' already exists. Skipping dump load."
+fi
+
+
+
 echo "start app"
 exec "$@"
