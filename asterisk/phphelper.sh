eed71e98bba2c5b3efb9705051004329c866bfd1
loki <gunlinux@ya.ru>
2024-05-25 23:13:04 +1000

    asterisk_revisited

--- asterisk/phphelper.sh
+++
@@ -1,5 +0,0 @@
-apt-get -y install lsb-release ca-certificates curl
-curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
-dpkg -i /tmp/debsuryorg-archive-keyring.deb
-sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-apt-get update
